1ad5f71257b0dc1b0f669f4a98df6eb2871e1d30
Christopher Salandra <christopher.salandra@rutgers.edu>
2018-10-09 17:22:00 -0400

    Add files via upload

---
+++ merge.cpp
@@ -0,0 +1,120 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <fstream> 
+using namespace std;
+
+
+
+
+
+
+
+// The structure for setting a linked list with a node
+struct Node
+{
+	int item;
+	Node* next;
+};
+
+// Function to print nodes in a given linked list 
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		printf("%d ", node->item);
+		node = node->next;
+	}
+}
+
+// Comparing two process to see what the order goes betwen the two
+Node* sortingMerge(Node* a, Node* b)
+{
+	Node* listResult = NULL;
+
+	// using for our base cases
+	if (b == NULL)
+	{
+		return (a);
+	}
+
+	else if (a == NULL)
+	{
+		return (b);
+	}
+	// comparing two list to help decided where the word is located
+
+
+	if (a->item <= b->item)
+	{
+		listResult = a;
+		listResult->next = sortingMerge(a->next, b);
+	}
+
+
+	else
+	{
+		listResult = b;
+		listResult->next = sortingMerge(a, b->next);
+	}
+
+	return listResult;
+}
+
+
+Node* mergeTheLists(Node* arr[], int lastone)
+{
+	// repeating this process until one list is left
+	while (lastone != 0)
+	{
+		j = lastone;
+		int i = 0;
+
+		// Comparing the pair 
+		while (i < j)
+		{
+			// merge List i with List j and storing in the list i
+			arr[i] = sortingMerge(arr[i], arr[j]);
+
+			// go to next pair
+			j--;
+			i++;
+
+			// Once all done, giving one last update to the list
+			if (i >= j)
+				lastone = j;
+		}
+	}
+
+	return arr[0];
+}
+
+// Utility function to create a new node. 
+Node *newNode(int item)
+{
+	struct Node *temp = new Node;
+
+	temp->item = item;
+
+	temp->next = NULL;
+
+	return temp;
+}
+
+
+
+
+
+
+
+}
---
+++ word.cpp
@@ -0,0 +1,84 @@
+// reducee words
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <fstream> 
+
+
+
+
+
+using namespace std;
+
+
+struct vecWord {
+	string word;
+	int freq;
+
+	vecWord(char w, int f)
+	{
+		word = w;
+		freq = f;
+	}
+
+}
+
+
+
+
+int main()
+{
+
+
+	// We next then have organize all the words in the string in alphabetical order
+
+	int low;
+	int high;
+
+	sort(num.begin() + low, num.end() + high);
+
+
+
+
+	// We first begin by counting the specific amount of each word
+
+
+	map<string, unsigned int> wordsCount;
+
+	// if we encounter the word for the first time
+	if (wordsCount.find(word) == wordsCount.end()) {
+
+		// need to assign the word in the string to 1
+		wordsCount[word] = 1;
+
+	}
+
+
+	// if we have already seen this word before
+	else {
+		// adding 1 to the amount it had before
+		wordsCount[word]++;
+	}
+
+
+
+
+
+
+
+
+
+
+}
+
