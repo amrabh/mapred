1302cffce21ca8c6f5875d0351a39414f3803d63
aojsd <michael.wu99@gmail.com>
2018-10-09 15:05:39 -0400

    Add files via upload

--- A1.cpp
+++ A1.cpp
@@ -13,13 +13,15 @@
 #include <thread>
 #include <stdlib.h>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
-vector<int> nums;
+vector<long> nums;
 vector<string> words;
+vector<map<string,int>> wo;
+vector<map<long,int>> nu;
 int fd;
-vector<vecWord> w;
 
 typedef struct wordhash{
     char word[80];
@@ -33,7 +35,13 @@ struct vecWord{
         word = string(w);
         freq = f;
     }
+    vecWord(string w, int f){
+        word = w;
+        freq = f;
+    }
 };
+
+vector<vecWord> w;
 
 void fill_words(string filename){
     ifstream file(filename);
@@ -41,7 +49,7 @@ void fill_words(string filename){
     
     while(getline(file,line)){
         size_t prev = 0, pos;
-        while((pos = line.find_first_of(" .,;:!-\t\n\v\f\r", prev)) != string::npos){
+        while((pos = line.find_first_of(" .,;:!-)\t\n\v\f\r", prev)) != string::npos){
             if(pos > prev){
                 if(line.substr(prev, pos-prev) != "—" && line.substr(prev, pos-prev) != "’"){
                     string s = line.substr(prev,pos-prev);
@@ -68,25 +76,30 @@ void fill_nums(string filename){
 
     while(getline(file,line)){
         size_t prev = 0, pos;
-        while((pos = line.find_first_of(" .,;:!-\t\n\v\f\r", prev)) != string::npos){
+        while((pos = line.find_first_of(" .,;:)!-\t\n\v\f\r", prev)) != string::npos){
             if(pos > prev){
-                nums.push_back(stoi(line.substr(prev,pos-prev)));
+                nums.push_back(stol(line.substr(prev,pos-prev)));
             }
             prev = pos + 1;
         }
         if(prev < line.length())
-            nums.push_back(stoi(line.substr(prev, string::npos)));
+            nums.push_back(stol(line.substr(prev, string::npos)));
     }
     file.close();
 }
 
 void process_func(char* func, int procs, int size){
     int status = 0;
+    int split;
     pid_t childID;
     char total[10];
     sprintf(total, "%d", size);
 
-    int split = size/procs;
+    if(procs > size){
+        split = 1;
+        procs = size;
+    }
+    else split = size/procs;
 
     // fork processes and send to function
     for(int i  = 0; i < procs - 1; i++){
@@ -117,7 +130,7 @@ void process_func(char* func, int procs, int size){
     else wait(&status);
 }
 
-void map(string infile, bool wordcount, int maps){
+void mapp(string infile, bool wordcount, int maps){
     // fill corresponding vectors
     if(wordcount){
         fill_words(infile);
@@ -145,7 +158,7 @@ void map(string infile, bool wordcount, int maps){
             perror("ftruncate");
             return;
         }
-        int *data = (int*)mmap(0, nums.size()*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        long *data = (long*)mmap(0, nums.size()*sizeof(long), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
         for(int i = 0; i < nums.size(); i++){
             data[i] = nums.at(i);
@@ -183,7 +196,7 @@ void reduce(string outfile, bool wordcount, int reds){
        file.close();
     }
     else{
-        int *data = (int*)mmap(0, nums.size()*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        long *data = (long*)mmap(0, nums.size()*sizeof(long), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         ofstream file(outfile);
         for(int i = 0; i < nums.size(); i++){
             file << data[i] << endl;
@@ -191,39 +204,141 @@ void reduce(string outfile, bool wordcount, int reds){
         file.close();
     }
 }
-void ECparse(string filename, bool wc){
-    if(wc){
-        ifstream file(filename);
-        string line;
-    
-        while(getline(file,line)){
-            size_t prev = 0, pos;
-            while((pos = line.find_first_of(" .,;:!-\t\n\v\f\r", prev)) != string::npos){
-                if(pos > prev){
-                    if(line.substr(prev, pos-prev) != "—" && line.substr(prev, pos-prev) != "’"){
-                        string s = line.substr(prev,pos-prev);
-                        transform(s.begin(), s.end(), s.begin(), ::tolower);
-                        
-                        if(find(v.begin(), v.end(), x) != v.end()
-                    }
-                }
-                prev = pos + 1;
-            }
-            if(prev < line.length()){
-                if(line.substr(prev, string::npos) != "—" && line.substr(prev, string::npos) != "’"){
-                    string s = line.substr(prev, string::npos);
+
+void wfill(int begin, int end, int index){
+    string line;
+    for(int i = begin; i < end; i++){
+        line = words[i];
+        size_t prev = 0, pos;
+        while((pos = line.find_first_of(" .,;:!-)\t\n\v\f\r", prev)) != string::npos){
+            if(pos > prev){
+                if(line.substr(prev, pos-prev) != "—" && line.substr(prev, pos-prev) != "’"){
+                    string s = line.substr(prev,pos-prev);
                     transform(s.begin(), s.end(), s.begin(), ::tolower);
                     
-                    if(w.count(s) == 1){
-                        w[s]++;
-                    }
-                    else w[s] = 1;
+                    // check if map already contains string
+                    if(wo[index].count(s) > 0)
+                         wo[index].at(s)++;
+                    else wo[index].emplace(s, 1);
                 }
             }
+            prev = pos + 1;
         }
-        file.close();
+        if(prev < line.length()){
+            if(line.substr(prev, string::npos) != "—" && line.substr(prev, string::npos) != "’"){
+                string s = line.substr(prev, string::npos);
+                transform(s.begin(), s.end(), s.begin(), ::tolower);
+                
+                if(wo[index].count(s) > 0)
+                    wo[index].at(s)++;
+                else wo[index].emplace(s, 1);
+            }
+        }
     }
-    else fill_nums(filename);
+}
+void nfill(int begin, int end, int index){
+    string line;
+    for(int i = begin; i < end; i++){
+        line = words[i];
+        size_t prev = 0, pos;
+        while((pos = line.find_first_of(" .,;:!-)\t\n\v\f\r", prev)) != string::npos){
+            if(pos > prev){
+                long k = stol(line.substr(prev,pos-prev));
+
+                // check is map already contains number
+                if(nu[index].count(k) > 0)
+                    nu[index].at(k)++;
+                else nu[index].emplace(k, 1);
+            }
+            prev = pos + 1;
+        }
+        
+        if(prev < line.length()){
+            long k = stol(line.substr(prev, string::npos));
+            if(nu[index].count(k) > 0)
+                nu[index].at(k)++;
+            else nu[index].emplace(k, 1);
+        }
+    }
+}
+
+void ECparse(string filename, bool wc, int t){
+    int split;
+    ifstream file(filename);
+    string line;
+    while(getline(file,line)){
+        words.push_back(line);
+    }
+    file.close();
+
+    if(t > words.size()){
+        t = words.size();
+        split = 1;
+    }
+    else split = words.size()/t;
+    
+    vector<thread> ts;
+    
+    if(wc){
+        for(int i = 0; i < t; i++){
+            map<string,int> m;
+            wo.push_back(m);
+        }
+        for(int i = 0; i < t - 1; i++){
+            ts.push_back(thread(wfill, i*split, (i+1)*split, i));
+        }
+        ts.push_back(thread(wfill, (t-1)*split, words.size(), t-1));
+        for(auto &t: ts){
+            t.join();
+        }
+    }
+    else{
+        for(int i = 0; i < t; i++){
+            map<long,int> m;
+            nu.push_back(m);
+        }
+        
+        for(int i = 0; i < t - 1; i++){
+            ts.push_back(thread(nfill, i*split, (i+1)*split, i));
+        }
+        ts.push_back(thread(nfill, (t-1)*split, words.size(), t-1));
+
+        for(auto &t: ts){
+            t.join();
+        }
+    }
+}
+
+// merge all maps into first map
+void merge(string outfile, bool wc){
+    ofstream file(outfile);
+    if(wc){
+        for(int i = 1; i < wo.size(); i++){
+            for(const auto &p: wo[i]){
+                if(wo[0].count(p.first) > 0)
+                    wo[0].at(p.first) += p.second;
+                else wo[0].emplace(p.first, p.second);
+            }
+        }
+        for(const auto &p: wo[0]){
+            file << p.first << " " << p.second << endl;
+        }
+    }
+
+    else{
+        for(int i = 1; i < nu.size(); i++){
+            for(const auto &p: nu[i]){
+                if(nu[0].count(p.first) > 0)
+                    nu[0].at(p.first) += p.second;
+                else nu[0].emplace(p.first, p.second);
+            }
+        }
+        for(const auto &p: nu[0]){
+            for(int i = 0; i < p.second; i++){
+                file << p.first << endl;
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv){
@@ -242,7 +357,7 @@ int main(int argc, char **argv){
         int reds = stoi(argv[8]);
         string infile = argv[10];
         string outfile = argv[12];
-        map(infile, wordcount, maps);
+        mapp(infile, wordcount, maps);
         reduce(outfile, wordcount, reds);
         shm_unlink("/shm");
     }
@@ -253,6 +368,9 @@ int main(int argc, char **argv){
         int threads = stoi(argv[6]);
         string infile = argv[8];
         string outfile = argv[10];
-        ECparse(infile, wordcount);
+        ECparse(infile, wordcount, threads);
+        //threadsort(threads, wordcount, outfile);
+        merge(outfile, wordcount);
+        
     }
 }
